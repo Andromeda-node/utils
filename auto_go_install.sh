af84b95f4d52ae1fc4463e08f6dcb8e6343b9d86
Andromeda <156133186+Andromeda-node@users.noreply.github.com>
2024-02-17 11:16:28 +0100

    Create auto_go_install.sh

---
+++ auto_go_install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Parse command-line options
+while getopts ":v:" flag; do
+  case "${flag}" in
+    v) V=${OPTARG} ;;
+    *) echo "WARN: unknown parameter: ${OPTARG}" ;;
+  esac
+done
+
+# Determine the Go version
+LATEST_VERSION=$(curl -s https://go.dev/dl/?mode=json | jq -r '.[0].version')
+VERSION="go${V:-$LATEST_VERSION}"
+
+# Determine the operating system and architecture
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+case "${ARCH}" in
+  x86_64) ARCH="amd64" ;;
+  aarch64) ARCH="arm64" ;;
+  *) echo "Unsupported architecture: ${ARCH}" >&2; exit 1 ;;
+esac
+
+# Download and install Go binaries
+curl -L -# -O "https://golang.org/dl/${VERSION}.${OS}-${ARCH}.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "${VERSION}.${OS}-${ARCH}.tar.gz"
+rm "${VERSION}.${OS}-${ARCH}.tar.gz"
+
+# Update PATH and GOPATH
+if ! grep -q "/usr/local/go/bin" "$HOME/.bash_profile"; then
+  echo "export PATH=\$PATH:/usr/local/go/bin" >> "$HOME/.bash_profile"
+fi
+if ! grep -q "GOPATH=\$HOME/go" "$HOME/.bash_profile"; then
+  echo "export GOPATH=\$HOME/go" >> "$HOME/.bash_profile"
+fi
+
+# Source .bash_profile
+source "$HOME/.bash_profile"
+
+# Display Go version
+go version
