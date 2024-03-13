c2e19fe65f5750b0a334140b85fc81e1a69188ea
Andromeda <156133186+Andromeda-node@users.noreply.github.com>
2024-03-13 18:22:23 +0100

    Update snapshot_finder.sh

--- snapshot_finder.sh
+++ snapshot_finder.sh
@@ -2,8 +2,11 @@
 
 # List of URLs to check
 urls=(
-    "https://files.andromedanode.co/${name}/snap_${name}.tar.lz4"
-    "https://${type}-files.itrocket.net/${name}/snap_${name}.tar.lz4"
+    "https://files.andromedanode.co/$PROJECT/snap_$PROJECT.tar.lz4"
+    "https://$NETWORK-files.itrocket.net/$PROJECT/snap_$PROJECT.tar.lz4"
+    "https://snapshots-$NETWORK.nodejumper.io/$PROJECT-$NETWORK/$PROJECT-$PROJECT_latest.tar.lz4"
+    "https://snap.nodex.one/$PROJECT-$NETWORK/$PROJECT-latest.tar.lz4"
+    "https://snapshots.kjnodes.com/$PROJECT-$NETWORK/snapshot_latest.tar.lz4"
     # Add more URLs here if needed
 )
 
@@ -11,8 +14,14 @@ urls=(
 for url in "${urls[@]}"; do
     if curl -s --head "$url" | head -n 1 | grep "200" > /dev/null; then
         # If the URL returns status 200, download and extract the archive
-        curl "$url" | lz4 -dc - | tar -xf - -C "$HOME/${path}"
-        # Exit loop since download was successful
-        break
+        if curl "$url" | lz4 -dc - | tar -xf - -C "$HOME/$DIRECTORY"; then
+            echo "Download and extraction successful from: $url"
+            # Exit loop since download was successful
+            break
+        else
+            echo "Failed to extract archive from: $url"
+        fi
+    else
+        echo "URL $url is not available."
     fi
 done
