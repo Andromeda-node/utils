c59dbb9539544f1cfe24b4375d85f12a3d302807
Andromeda <156133186+Andromeda-node@users.noreply.github.com>
2024-04-24 08:26:13 +0200

    Update oraichain_upgrade.sh

--- blockheight_upgrade/oraichain_upgrade.sh
+++ blockheight_upgrade/oraichain_upgrade.sh
@@ -1,1 +1,11 @@
+sudo systemctl stop oraid
 
+# Download new binary
+cd $HOME 
+rm -rf orai 
+git clone https://github.com/oraichain/orai 
+cd orai/orai 
+git checkout v0.41.8
+make install
+
+sudo systemctl start oraid
